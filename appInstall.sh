d22163c98e85a770c848913f8d5cdff269a649eb
Michel Chanthavong <michel.chanthavong@gmail.com>
2022-11-08 23:19:29 +0100

    feat: Initial commit

---
+++ appInstall.sh
@@ -0,0 +1,17 @@
+echo "Starting app_install.sh"
+
+which -s brew
+if [[ $? != 0 ]] ; then
+    echo "Installing Brew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    echo "Brew has been installed !"
+
+else
+    echo "Good news ! Brew already installed and will be updated"
+    brew update
+fi
+
+# Brew bundle will execute the BrewFile
+brew bundle
+
+echo "app_install.sh finished"
