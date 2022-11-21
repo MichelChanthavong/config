6d9ca752d2bc5fa133ac30f82410215daf37847a
MichelChanthavong <michel.chanthavong@backmarket.com>
2022-11-21 10:04:05 +0100

    feat: Add SSH checks on the appInstall script

--- appInstall.sh
+++ appInstall.sh
@@ -1,18 +1,27 @@
 echo "Starting app_install.sh"
 
+########## Install all application through Brew ##########
 which -s brew
 if [[ $? != 0 ]] ; then
     echo "Installing Brew..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     echo "Brew has been installed !"
-
-else
-    echo "Good news ! Brew already installed and will be updated"
-    brew update
 fi
 
 echo "Start installing your favorite apps"
-# Brew bundle will execute the BrewFile
 brew bundle
+
+########## Create SSH key ##########
+
+RSA_SSH_KEY_TYPE=~/.ssh/id_rsa.pub
+ECDSA_SSH_KEY_TYPE=~/.ssh/id_ecdsa.pub
+ED25519_SSH_KEY_TYPE=~/.ssh/id_ed25519.pub
+
+if [[ ! -f $RSA_SSH_KEY_TYPE ]] || [[ ! -f $ED25519_SSH_KEY_TYPE ]] || [[ ! -f $ECDSA_SSH_KEY_TYPE ]] ; then
+    # TODO Add a "Want to create a SSH key ?" 
+    echo  "Seems that there is not SSH key on your computer. Please enter your GitHub email address"
+    read YOUR_GITHUB_MAIL
+    ssh-keygen -t ed25519 -C $YOUR_GITHUB_MAIL 
+fi
 
 echo "app_install.sh finished"
